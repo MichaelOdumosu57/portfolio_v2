271454ab7f0afc7762dd007dab7f3358d0d55c5e
No Reply <21322737+MichaelOdumosu57@users.noreply.github.com>
2022-08-14 12:22:43 -0400

    [UPDATE] replaced hardcoded i18n

--- apps/zero/frontend/AngularAppCurrent/src/app/shared/wml-components/wml-fields/wml-fields.module.ts
+++ apps/zero/frontend/AngularAppCurrent/src/app/shared/wml-components/wml-fields/wml-fields.module.ts
@@ -2,14 +2,12 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { WmlFieldComponent } from './wml-fields.component';
 import { WmlLabelComponent } from './wml-label/wml-label.component';
-import { WmlErrorComponent } from './wml-error/wml-error.component';
 
 
 @NgModule({
   declarations: [
     WmlFieldComponent,
     WmlLabelComponent,
-    WmlErrorComponent,
   ],
   imports: [
     CommonModule,
